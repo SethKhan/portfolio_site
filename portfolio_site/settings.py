f70ab21b560d8cbe172e12beea980d92cfcb81ad
SethKhan <SethKhan27@gmail.com>
2022-01-11 18:32:59 -0500

    added blog, portfolio, movieticket

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quote_generator',
+    'portfolio',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +107,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+# STATIC_DIRS = [
+#     BASE_DIR / 'quote_generator' / 'static',
+#     BASE_DIR / 'movie_booking' / 'static',
+# ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
