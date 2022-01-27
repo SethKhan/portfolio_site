8bdc11e70bc3e8b3d7dcdf7a98ab0383348342c0
SethKhan <SethKhan27@gmail.com>
2022-01-27 15:42:59 -0500

    Removed Old Files

--- portfolio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-# This class configures the portfolio app. The name object myst be the same name as the foder name
-class PortfolioConfig(AppConfig):
-    name = 'portfolio'
--- quote_generator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-# Quote generator app is modeled in this class.
-class QuoteGeneratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quote_generator'
